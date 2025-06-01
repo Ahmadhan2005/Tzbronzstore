b2e2ba305d31101a68ca6f3c67e2c10308ef60ce
Ahmadhan Syafiere <ahmadansyafiere@gmail.com>
2025-06-01 09:12:30 +0700

    Update page.js

--- app/product/game/blood-strike/page.js
+++ app/product/game/blood-strike/page.js
@@ -22,7 +22,7 @@ const bloodStrikePassOptions = [
 // Data info game
 const bloodStrikeGameInfo = {
   name: "Blood Strike",
-  image: "/Blood Strike.png",
+  image: "/Blood strike.png",
   desc: "Top up Gold dan Pass Blood Strike untuk upgrade akun, skin, dan item in-game lainnya. Harga terbaik & proses instan di tzbronzstore.id.",
 };
 
